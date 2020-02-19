934bc5674180bbc11d3b7d18231de1ba69cfa873
dongphan1988 <phantrongdong15488@gmail.com>
2020-02-19 13:23:00 +0700

    khai bao su dung bien

---
+++ src/khaibao_sdung_bien.java
@@ -0,0 +1,26 @@
+//import java.util.*;
+//import java.lang.*;
+
+public class khaibao_sdung_bien {
+
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d =20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha noi";
+        String ss = "Ha noi1";
+
+        System.out.println("i = " + i + "\n");
+        System.out.println("f = " + f + "\n");
+        System.out.println("d = " + d + "\n");
+        System.out.println("b = " + b + "\n");
+        System.out.println("c = " + c + "\n");
+        System.out.println("s = " + s + "\n");
+        if(s.equals(ss)){
+            System.out.println("-----Bingo------");
+        }
+    }
+
+}
